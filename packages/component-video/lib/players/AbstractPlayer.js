1e2cf964c589b540204bf656c4a65419943735e3
Andy Kenward <andykenward@stinkdigital.com>
2016-04-08 16:03:04 +0100

    BUILD

--- packages/component-video/lib/players/AbstractPlayer.js
+++ packages/component-video/lib/players/AbstractPlayer.js
@@ -12,77 +12,11 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var AbstractPlayer = function () {
 	function AbstractPlayer() {
-		var _this = this;
-
 		var mOptions = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
 
 		_classCallCheck(this, AbstractPlayer);
 
-		this._onVideoMetadata = function () {
-			var e = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
-
-			_this._addToDom();
-			if (_this._options.resize) _this._onVideoResize();
-			if (_this._callbackMetadata) _this._callbackMetadata(e);
-		};
-
-		this._onVideoCanPlayThrough = function (e) {
-			_this._player.removeEventListener('canplaythrough', _this._onVideoCanPlayThrough);
-			_this.videoReady = true;
-			if (_this.autoplay) _this.play();
-			if (_this._callbackCanPlay) _this._callbackCanPlay(e);
-		};
-
-		this._onVideoEnd = function (e) {
-			_this.playing = false;
-			_this.paused = true;
-			/* to loop a youtube video check if loop exist */
-			if (!_this._player.loop && _this.looping) _this.play();
-			if (_this._callbackEnd) _this._callbackEnd(e);
-		};
-
-		this._onVideoError = function (e) {
-			if (_this._callbackError) _this._callbackError(e);
-			throw new Error('Error - component-video - ', e);
-		};
-
-		this._onVideoPlay = function (e) {
-			_this.playing = true;
-			_this.paused = false;
-			_this.autoPaused = false;
-			if (_this._callbackPlay) _this._callbackPlay(e);
-		};
-
-		this._onVideoPause = function (e) {
-			_this.playing = false;
-			_this.paused = true;
-			if (_this._callbackPause) _this._callbackPause(e);
-		};
-
-		this._onVideoProgress = function (e) {
-			if (_this._callbackProgress) _this._callbackProgress(e);
-		};
-
-		this._onVideoResize = function () {
-			var el = _this.el;
-			if (!el || !window) {
-				throw new Error('player isnt in dom. So cant resize');
-			}
-			var width = _this.width;
-			var height = _this.height;
-			var areaWidth = _this._options.el ? _this._options.el.offsetWidth : window.innerWidth;
-			var areaHeight = _this._options.el ? _this._options.el.offsetHeight : window.innerHeight;
-			var scale = Math.max(areaWidth / width, areaHeight / height);
-
-			el.style.width = Math.ceil(width * scale) + 'px';
-			el.style.height = Math.ceil(height * scale) + 'px';
-			el.style.top = Math.ceil((areaHeight - height * scale) / 2) + 'px';
-			el.style.left = Math.ceil((areaWidth - width * scale) / 2) + 'px';
-		};
-
-		this._onTimeUpdate = function (e) {
-			if (_this._callbackTimeUpdate) _this._callbackTimeUpdate(e);
-		};
+		_initialiseProps.call(this);
 
 		this._options = mOptions;
 
@@ -93,7 +27,14 @@ var AbstractPlayer = function () {
 		var loop = _options$loop === undefined ? false : _options$loop;
 		var _options$resize = _options.resize;
 		var resize = _options$resize === undefined ? false : _options$resize;
+		var _options$width = _options.width;
+		var width = _options$width === undefined ? '100%' : _options$width;
+		var _options$height = _options.height;
+		var height = _options$height === undefined ? '100%' : _options$height;
 
+
+		this._options.width = width;
+		this._options.height = height;
 
 		this.autoplay = autoplay;
 		this.looping = loop;
@@ -137,6 +78,12 @@ var AbstractPlayer = function () {
 		key: 'seek',
 		value: function seek(time) {
 			this.currentTime = time;
+		}
+	}, {
+		key: 'setSize',
+		value: function setSize(width, height) {
+			this.width = width;
+			this.height = height;
 		}
 	}, {
 		key: '_addToDom',
@@ -209,11 +156,17 @@ var AbstractPlayer = function () {
 		}
 	}, {
 		key: 'width',
+		set: function set(value) {
+			this.el.style.width = value;
+		},
 		get: function get() {
 			return this.el.videoWidth;
 		}
 	}, {
 		key: 'height',
+		set: function set(value) {
+			this.el.style.height = value;
+		},
 		get: function get() {
 			return this.el.videoHeight;
 		}
@@ -221,6 +174,78 @@ var AbstractPlayer = function () {
 
 	return AbstractPlayer;
 }();
+
+var _initialiseProps = function _initialiseProps() {
+	var _this = this;
+
+	this._onVideoMetadata = function () {
+		var e = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+		if (!_this.resize) {
+			_this.setSize(_this._options.width, _this._options.height);
+		}
+		_this._addToDom();
+		if (_this._options.resize) _this._onVideoResize();
+		if (_this._callbackMetadata) _this._callbackMetadata(e);
+	};
+
+	this._onVideoCanPlayThrough = function (e) {
+		_this._player.removeEventListener('canplaythrough', _this._onVideoCanPlayThrough);
+		_this.videoReady = true;
+		if (_this.autoplay) _this.play();
+		if (_this._callbackCanPlay) _this._callbackCanPlay(e);
+	};
+
+	this._onVideoEnd = function (e) {
+		_this.playing = false;
+		_this.paused = true;
+		/* to loop a youtube video check if loop exist */
+		if (!_this._player.loop && _this.looping) _this.play();
+		if (_this._callbackEnd) _this._callbackEnd(e);
+	};
+
+	this._onVideoError = function (e) {
+		if (_this._callbackError) _this._callbackError(e);
+		throw new Error('Error - component-video - ', e);
+	};
+
+	this._onVideoPlay = function (e) {
+		_this.playing = true;
+		_this.paused = false;
+		_this.autoPaused = false;
+		if (_this._callbackPlay) _this._callbackPlay(e);
+	};
+
+	this._onVideoPause = function (e) {
+		_this.playing = false;
+		_this.paused = true;
+		if (_this._callbackPause) _this._callbackPause(e);
+	};
+
+	this._onVideoProgress = function (e) {
+		if (_this._callbackProgress) _this._callbackProgress(e);
+	};
+
+	this._onVideoResize = function () {
+		var el = _this.el;
+		if (!el || !window) {
+			throw new Error('player isnt in dom. So cant resize');
+		}
+		var width = _this.width;
+		var height = _this.height;
+		var areaWidth = _this._options.el ? _this._options.el.offsetWidth : window.innerWidth;
+		var areaHeight = _this._options.el ? _this._options.el.offsetHeight : window.innerHeight;
+		var scale = Math.max(areaWidth / width, areaHeight / height);
+
+		_this.setSize(Math.ceil(width * scale) + 'px', Math.ceil(height * scale) + 'px');
+		el.style.top = Math.ceil((areaHeight - height * scale) / 2) + 'px';
+		el.style.left = Math.ceil((areaWidth - width * scale) / 2) + 'px';
+	};
+
+	this._onTimeUpdate = function (e) {
+		if (_this._callbackTimeUpdate) _this._callbackTimeUpdate(e);
+	};
+};
 
 exports.default = AbstractPlayer;
 module.exports = exports['default'];
