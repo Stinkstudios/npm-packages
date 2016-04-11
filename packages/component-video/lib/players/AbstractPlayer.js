8ba8f02294a49f0def0ba96b4bd4f921b93ae7bf
Andy Kenward <andykenward@stinkdigital.com>
2016-04-11 14:13:09 +0100

    BUILD

--- packages/component-video/lib/players/AbstractPlayer.js
+++ packages/component-video/lib/players/AbstractPlayer.js
@@ -31,12 +31,12 @@ var AbstractPlayer = function () {
 		var width = _options$width === undefined ? '100%' : _options$width;
 		var _options$height = _options.height;
 		var height = _options$height === undefined ? '100%' : _options$height;
-		var _options$pageVisibilt = _options.pageVisibilty;
-		var pageVisibilty = _options$pageVisibilt === undefined ? false : _options$pageVisibilt;
+		var _options$pageVisibili = _options.pageVisibility;
+		var pageVisibility = _options$pageVisibili === undefined ? false : _options$pageVisibili;
 
 
 		this._options.resize = this._options.youtubeId ? false : resize;
-		this._options.pageVisibilty = pageVisibilty;
+		this._options.pageVisibility = pageVisibility;
 		this._options.width = width;
 		this._options.height = height;
 
@@ -92,7 +92,8 @@ var AbstractPlayer = function () {
 		key: '_addToDom',
 		value: function _addToDom() {
 			if (!this._options.el) document.body.appendChild(this.el);else this._options.el.appendChild(this.el);
-		}
+		} // eslint-disable-line max-len
+
 	}, {
 		key: 'autoplay',
 		set: function set(value) {
@@ -239,10 +240,7 @@ var _initialiseProps = function _initialiseProps() {
 		var areaWidth = _this._options.el ? _this._options.el.offsetWidth : window.innerWidth;
 		var areaHeight = _this._options.el ? _this._options.el.offsetHeight : window.innerHeight;
 		var scale = Math.max(areaWidth / width, areaHeight / height);
-
-		_this.setSize(Math.ceil(width * scale) + 'px', Math.ceil(height * scale) + 'px');
-		el.style.top = Math.ceil((areaHeight - height * scale) / 2) + 'px';
-		el.style.left = Math.ceil((areaWidth - width * scale) / 2) + 'px';
+		el.style.cssText = 'width:' + Math.ceil(width * scale) + 'px; height:' + Math.ceil(height * scale) + 'px; position:absolute; top:' + Math.ceil((areaHeight - height * scale) / 2) + 'px; left:' + Math.ceil((areaWidth - width * scale) / 2) + 'px;';
 	};
 
 	this._onTimeUpdate = function (e) {
