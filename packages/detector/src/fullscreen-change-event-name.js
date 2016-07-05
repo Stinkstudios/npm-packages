3ec5b9a1f60cfa02fb8a970682a040c753829d47
Elie Génard <elie@stinkdigital.com>
2016-07-05 11:31:06 +0100

    Fix fullscreen event names

--- packages/detector/src/fullscreen-change-event-name.js
+++ packages/detector/src/fullscreen-change-event-name.js
@@ -1,6 +1,4 @@
-
 var FULLSCREEN_CHANGE_EVENT_NAME = (function () {
-	var el = document.createElement('fakeelement');
 	var fullscreenchanges = {
 		fullscreenchange: 'fullscreenchange',
 		MozFullscreenchange: 'mozfullscreenchange',
@@ -8,8 +6,10 @@ var FULLSCREEN_CHANGE_EVENT_NAME = (function () {
 	};
 	var fullscreenchange;
 	for (var t in fullscreenchanges) {
-		if (el.style[t] !== undefined) {
-			fullscreenchange = fullscreenchanges[t];
+		var name = fullscreenchanges[t];
+		var eventName = 'on' + name;
+		if (eventName in document) {
+			fullscreenchange = name;
 			break;
 		}
 	}
