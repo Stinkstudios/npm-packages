cb7bdc49ef59e223ca1972d626bc39d42f3a62b3
Elie Génard <elie@stinkdigital.com>
2016-08-05 13:11:29 +0100

    Fix has-ogg-vorbis.js

--- packages/detector/src/has-ogg-vorbis.js
+++ packages/detector/src/has-ogg-vorbis.js
@@ -1,7 +1,15 @@
 // https://github.com/Modernizr/Modernizr/blob/master/feature-detects/audio.js
 var HAS_OGG_VORBIS = (function () {
   var elem = document.createElement('audio');
-  elem.canPlayType('audio/ogg; codec="vorbis"').replace(/^no$/, '');
+  var bool = false;
+  try {
+    bool = !!elem.canPlayType;
+    if (bool) {
+      bool = new Boolean(bool);
+      bool = elem.canPlayType('audio/ogg; codec="vorbis"').replace(/^no$/, '');
+    }
+  } catch (e) { }
+  return bool;
 })();
 
 module.exports = HAS_OGG_VORBIS;
