5d271d4d1d5913b8294b6e237e483b6550dcde83
Andre Venancio <info@andrevenancio.com>
2016-11-25 13:01:16 +0000

    Merge branch 'release/adding_versioning'

--- packages/detector/webpack.config.js
+++ packages/detector/webpack.config.js
@@ -1,19 +1,23 @@
 var webpack = require('webpack')
 var path = require('path')
+var pkg = require('./package.json');
 
 module.exports = {
-  context: path.join(__dirname, './src'),
-  entry: {
-    js: './index.js'
-  },
-  output: {
-    path: path.join(__dirname, './dist'),
-    filename: 'detector.min.js',
-  },
-  resolve: {
-    extensions: ['', '.js']
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({minimize: true})
-  ]
-}
+    context: path.join(__dirname, './src'),
+    entry: {
+        js: './index.js'
+    },
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'detector.min.js',
+    },
+    resolve: {
+        extensions: ['', '.js']
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __VERSION__: JSON.stringify(pkg.version),
+        }),
+        new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ]
+};
