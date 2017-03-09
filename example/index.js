1ea5f0571241f9ee21d4813dea6e21e81562bf61
Daisy Smith <daisymarie128@gmail.com>
2017-03-09 16:16:27 +0000

    demo component WIP

---
+++ example/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Fullbleed from '../components/fullbleed/fullbleed';
+
+ReactDOM.render(
+	<div>
+		<Fullbleed addClass="home-page__bg" width={200} height={300} ratio={'1:1'}>
+			<img src="https://placekitten.com/300/201" alt="" />
+		</Fullbleed>
+	</div>, document.getElementById('container'));
