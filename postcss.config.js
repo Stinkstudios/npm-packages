89b172be2d586d14db9e0316b55cb36cfeffeeb0
Daisy Smith <daisymarie128@gmail.com>
2017-03-09 17:42:25 +0000

    base styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,15 @@
+const path = require('path');
+
 module.exports = {
 	parser: 'postcss-scss',
 	plugins: {
 		'postcss-import': {
 			path: [
+				path.join(process.cwd()),
+				path.join(process.cwd(), 'base'),
+				path.join(process.cwd(), 'base', 'vars'),
+				path.join(process.cwd(), 'base', 'utilities'),
+				path.join(process.cwd(), 'base', 'objects'),
 			],
 		},
 		'postcss-for': {},
