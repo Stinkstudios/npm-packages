7da4cbc45a6f2d932c5933e58e0825049083d4c9
Daisy Smith <daisymarie128@gmail.com>
2017-03-09 16:04:03 +0000

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,17 @@
 /* eslint comma-dangle: 0 */
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const path = require('path');
+
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 	inject: 'body',
 	template: `${__dirname}/example/index.html`,
 	output: `${__dirname}/dist/index.html`
 });
 
-const prod = process.env.NODE_ENV === 'production';
-
-function getEntrySources() {
-	if (!prod) {
-		return ['./example/index.js'];
-	}
-	return ['components.js'];
-}
-
 module.exports = {
 	entry: {
-		app: getEntrySources()
+		app: './example/index.js'
 	},
-	devtool: 'cheap-module-eval-source-map',
 	stats: {
 		cached: false,
 		cachedAssets: false,
@@ -39,30 +29,22 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js'
 	},
-  module: {
+	module: {
 		rules: [
 			{
 				test: /\.js$/,
-        loader: 'babel-loader',
+				loader: 'babel-loader',
 				exclude: /(node_modules|dist)/,
-        query: {
-          cacheDirectory: true,
-        }
+				query: {
+					cacheDirectory: true,
+				}
 			}, {
 				test: /\.scss$/,
 				use: ['style-loader', 'css-loader', 'sass-loader']
 			}
 		]
 	},
-	plugins: prod ? [
-		new webpack.optimize.OccurrenceOrderPlugin(),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				screw_ie8: true,
-				warnings: false
-			}
-		})
-	] : [
-    HTMLWebpackPluginConfig
-  ]
+	plugins: [
+		HTMLWebpackPluginConfig
+	]
 };
