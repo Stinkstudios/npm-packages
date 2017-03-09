33992682cc709802709a3746b33b7c3013ee505d
Daisy Smith <daisymarie128@gmail.com>
2017-03-09 17:39:00 +0000

    add preloader

--- components.js
+++ components.js
@@ -1,7 +1,9 @@
 import Fullbleed from './components/fullbleed/fullbleed';
+import Preloader from './components/preloader/preloader';
 
 class Components {}
 
 Components.Fullbleed = Fullbleed;
+Components.Preloader = Preloader;
 
 export default Components;
---
+++ components/preloader/preloader.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+
+import './preloader.css';
+
+const Preloader = () => (
+	<div className="spinner">
+		{[...Array(4)].map((line, i) => (
+			<div key={i} className={`spinner__line spinner__line--${i + 1}`}>
+				<div className="spinner__line-cog">
+					<div className="spinner__line-cog-inner spinner__line-cog-inner--left" />
+				</div>
+				<div className="spinner__line-ticker">
+					<div className="spinner__line-cog-inner spinner__line-cog-inner--center" />
+				</div>
+				<div className="spinner__line-cog">
+					<div className="spinner__line-cog-inner spinner__line-cog-inner--right" />
+				</div>
+			</div>
+		))}
+	</div>
+);
+
+
+export default Preloader;
