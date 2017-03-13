9ca408b3a0d4ed5570c4def60b2f594afaa9b0c5
Daisy Smith <daisymarie128@gmail.com>
2017-03-13 16:30:17 +0000

    add functions for creating and passing form data

--- components/form/form.js
+++ components/form/form.js
@@ -4,36 +4,63 @@ import Input from '../input/input';
 import './form.css';
 
 class Form extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { formData: {} };
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
 	componentWillMount() {
+		const { fields } = this.props;
+		const formData = {};
+		fields.map((field) => {
+			formData[field.uid] = '';
+			return formData;
+		});
+
+		this.setState({ formData });
+	}
+
+	handleFieldChange = (value, name) => {
+		// const newState = {};
+		// newState[fieldId] = value;
+		const newData = (JSON.parse(JSON.stringify(this.state.formData)));
+		newData[name] = value;
+		this.setState({ formData: newData });
+	}
+
+	handleSubmit(e) {
+		const { onSubmit } = this.props;
+		e.preventDefault();
+		onSubmit();
 	}
 
 	render() {
-		const { fields, onSubmit } = this.props;
+		const { fields } = this.props;
 
 		return (
 			<form
 				className="o-form"
 				role="form"
 				autoComplete
+				onSubmit={this.handleSubmit}
 			>
-				<div className="form__description">This is the descriptio for the form .... Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+				<p className="form__description">This is the description for the form .... Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 				{fields.map((field) => (
 					<Input
 						key={field.name}
+						id={field.uid}
 						inputType={field.inputType}
 						inputMode={field.inputMode}
 						title={field.title}
 						name={field.name}
 						content={field.content}
 						placeholder={field.placeholder}
+						updateValue={(value, name) => { this.handleFieldChange(value, name); }}
 					/>
 				))}
-				<button
-					className="form__submit"
-					onClick={() => onSubmit()}
-				>
-					Submit
-				</button>
+				<input type="submit" value="Submit" />
 			</form>
 		);
 	}
--- components/input/input.js
+++ components/input/input.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './input.css';
 
-const Input = (props) => (
-	<input
-		className={`form__input ${props.halfSize ? 'form__input--half' : ''}`}
-		name={props.name}
-		type={props.inputType}
-		placeholder={props.placeholder}
-		autoComplete={props.inputAutoComplete}
-		aria-required={props.ariaRequired}
-	/>
-);
+class Input extends Component {
+	updateInput = (e) => {
+		this.props.updateValue(e.target.value, e.target.name);
+	}
+
+	render() {
+		const {
+			halfSize,
+			name,
+			inputType,
+			placeholder,
+			inputAutoComplete,
+			ariaRequired,
+		} = this.props;
+		return (
+			<input
+				className={`form__input ${halfSize ? 'form__input--half' : ''}`}
+				name={name}
+				type={inputType}
+				placeholder={placeholder}
+				autoComplete={inputAutoComplete}
+				aria-required={ariaRequired}
+				onChange={this.updateInput}
+			/>
+		);
+	}
+}
 
 Input.propTypes = {
-	inputType: React.PropTypes.oneOf(['text', 'number', 'email', 'search', 'tel', 'url']).isRequired,
+	inputType: React.PropTypes.oneOf(['text', 'number', 'email', 'search', 'tel', 'url', 'checkbox', 'radio']).isRequired,
 	inputAutoComplete: React.PropTypes.oneOf(['', 'honorific-prefix', 'name', 'given-name', 'family-name', 'email', 'username', 'street-address', 'address-line1', 'address-line2', 'country', 'postal-code', 'bday', 'sex', 'tel', 'url']),
 	name: React.PropTypes.string.isRequired,
 	placeholder: React.PropTypes.string,
 	halfSize: React.PropTypes.bool,
 	ariaRequired: React.PropTypes.string,
+	updateValue: React.PropTypes.func.isRequired,
 };
 
 
