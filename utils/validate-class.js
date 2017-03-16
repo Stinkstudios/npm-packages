d1b0941a569567ef3ea24cdcca5195dd9437b372
James Oldfield <hi@james-oldfield.co>
2017-03-16 10:58:50 +0000

    throw error in validate util if non-string

--- utils/validate-class.js
+++ utils/validate-class.js
@@ -1,6 +1,9 @@
 export function validateClassNames(props, propName, componentName = 'ANONYMOUS') {
 	if (props[propName]) {
-		const names = props[propName].split(' ');
+		const classname = props[propName];
+		if (typeof classname !== 'string') throw new Error(`Classnames must be a string, got ${typeof classname}`);
+
+		const names = classname.split(' ');
 		const re = new RegExp(/[A-Za-z]-|__/);
 		const error = `\`${propName}\` on ${componentName}. Values must be either` +
 		' namespaced Block names (https://github.com/stinkstudios/guidelines/tree/master/css#namespaces)' +
