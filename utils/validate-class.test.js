e36240d12c5084c1cd71f9bf9fe1f2c807af6239
James Oldfield <hi@james-oldfield.co>
2017-03-16 10:52:04 +0000

    add assertions for valid classnames + invalidones

---
+++ utils/validate-class.test.js
@@ -0,0 +1,12 @@
+import { validateClassNames } from './validate-class';
+
+const error = '\`addClass\` on ANONYMOUS. Values must be either namespaced Block names (https://github.com/stinkstudios/guidelines/tree/master/css#namespaces) or Element names (http://getbem.com/naming/). Other Component names NOT allowed.';
+
+test('validation of valid classnames', () => {
+	expect(validateClassNames({'addClass': 'block__element'}, 'addClass')).toBe(null)
+	expect(validateClassNames({'addClass': 'block__element--modifier'}, 'addClass')).toBe(null)
+});
+
+test('validation of invalid classnames', () => {
+	expect(() => validateClassNames({'addClass': '4'}, 'addClass').toThrow(error))
+});
