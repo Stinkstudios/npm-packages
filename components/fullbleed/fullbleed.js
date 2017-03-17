49fbbab50ae8ac4b4050e3d9095013877f739e45
Daisy Smith <daisymarie128@gmail.com>
2017-03-17 12:42:24 +0000

    lint

--- components/fullbleed/fullbleed.js
+++ components/fullbleed/fullbleed.js
@@ -4,49 +4,48 @@ import { validateClassNames } from '../../utils/validate-class';
 import './fullbleed.css';
 
 class Fullbleed extends Component {
-	componentWillMount() {
-		const { ratio } = this.props;
-		const ratioValues = ratio.split(':');
-		this.childRatio = ratioValues[0] / ratioValues[1];
-	}
+  componentWillMount() {
+    const { ratio } = this.props;
+    const ratioValues = ratio.split(':');
+    this.childRatio = ratioValues[0] / ratioValues[1];
+  }
 
-	renderChildren(blockName) {
-		const { children } = this.props;
-		return React.Children.map(children, (child) =>
-			React.cloneElement(child, {
-				className: `${blockName}__item o-media__${child.type}`,
-			}),
-		);
-	}
+  renderChildren(blockName) {
+    const { children } = this.props;
+    return React.Children.map(children, child =>
+      React.cloneElement(child, {
+        className: `${blockName}__item o-media__${child.type}`
+      }));
+  }
 
-	render() {
-		const { addClass, width, height } = this.props;
+  render() {
+    const { addClass, width, height } = this.props;
 
-		const blockName = 'fullbleed';
-		const classes = [blockName];
-		if (addClass) classes.push(addClass);
-		const browserRatio = width / height;
-		if (browserRatio < this.childRatio) classes.push('is-below-ratio');
-		classes.push('o-media');
+    const blockName = 'fullbleed';
+    const classes = [blockName];
+    if (addClass) classes.push(addClass);
+    const browserRatio = width / height;
+    if (browserRatio < this.childRatio) classes.push('is-below-ratio');
+    classes.push('o-media');
 
-		return (
-			<div className={classes.join(' ')}>
-				{this.renderChildren(blockName)}
-			</div>
-		);
-	}
+    return (
+      <div className={classes.join(' ')}>
+        {this.renderChildren(blockName)}
+      </div>
+    );
+  }
 }
 
 Fullbleed.propTypes = {
-	addClass: validateClassNames,
-	children: PropTypes.node.isRequired,
-	ratio: PropTypes.string.isRequired,
-	width: PropTypes.number.isRequired,
-	height: PropTypes.number.isRequired,
+  addClass: validateClassNames,
+  children: PropTypes.node.isRequired,
+  ratio: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired
 };
 
 Fullbleed.defaultProps = {
-	addClass: null,
+  addClass: null
 };
 
 export default Fullbleed;
