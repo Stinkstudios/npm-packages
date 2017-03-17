5597c2a096945b1bd6e794bfd313f500dd549b79
Daisy Smith <daisymarie128@gmail.com>
2017-03-17 12:42:01 +0000

    add progress bar

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,67 @@
 /* eslint comma-dangle: 0 */
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const path = require('path');
 
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-	inject: 'body',
-	template: `${__dirname}/example/index.html`,
-	output: `${__dirname}/dist/index.html`
+  inject: 'body',
+  template: `${__dirname}/example/index.html`,
+  output: `${__dirname}/dist/index.html`
 });
 
 module.exports = () => ({
-	entry: {
-		app: './example/index.js'
-	},
-	stats: {
-		cached: false,
-		cachedAssets: false,
-		chunkModules: false,
-		chunks: false,
-		colors: true,
-		errorDetails: true,
-		hash: false,
-		progress: true,
-		reasons: false,
-		timings: true,
-		version: false
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /(node_modules|dist)/,
-				query: {
-					cacheDirectory: true
-				}
-			}, {
-				test: /\.css$/,
-				exclude: /node_modules/,
-				loader: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						query: {
-							modules: false,
-							sourceMap: true,
-							importLoaders: 1,
-						},
-					},
-					{
-						loader: 'postcss-loader',
-						query: {
-							config: path.join(process.cwd(), './postcss.config.js')
-						},
-					},
-				],
-			}
-		]
-	},
-	plugins: [
-		HTMLWebpackPluginConfig
-	]
+  entry: {
+    app: './example/index.js'
+  },
+  stats: {
+    cached: false,
+    cachedAssets: false,
+    chunkModules: false,
+    chunks: false,
+    colors: true,
+    errorDetails: true,
+    hash: false,
+    progress: true,
+    reasons: false,
+    timings: true,
+    version: false
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|dist)/,
+        query: {
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            query: {
+              modules: false,
+              sourceMap: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            query: {
+              config: path.join(process.cwd(), './postcss.config.js')
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new ProgressBarPlugin(), HTMLWebpackPluginConfig]
 });
