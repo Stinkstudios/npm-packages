fcf2d35c0077390924a552ae54fbbb6d1723854b
Daisy Smith <daisymarie128@gmail.com>
2017-03-17 12:38:12 +0000

    update lint configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,28 @@
 const path = require('path');
 
 module.exports = {
-	parser: 'postcss-scss',
-	plugins: {
-		'postcss-import': {
-			path: [
-				path.join(process.cwd()),
-				path.join(process.cwd(), 'base'),
-				path.join(process.cwd(), 'base', 'vars'),
-				path.join(process.cwd(), 'base', 'utilities'),
-				path.join(process.cwd(), 'base', 'objects'),
-			],
-		},
-		'postcss-for': {},
-		'postcss-each': {},
-		'postcss-mixins': {},
-		'postcss-simple-vars': {},
-		'postcss-nested': {},
-		'postcss-conditionals': {},
-		'postcss-math': {},
-		'postcss-focus': {},
-		'postcss-cssnext': {},
-		'postcss-reporter': {
-			clearMessages: true,
-		},
-	},
+  parser: 'postcss-scss',
+  plugins: {
+    'postcss-import': {
+      path: [
+        path.join(process.cwd()),
+        path.join(process.cwd(), 'base'),
+        path.join(process.cwd(), 'base', 'vars'),
+        path.join(process.cwd(), 'base', 'utilities'),
+        path.join(process.cwd(), 'base', 'objects')
+      ]
+    },
+    'postcss-for': {},
+    'postcss-each': {},
+    'postcss-mixins': {},
+    'postcss-simple-vars': {},
+    'postcss-nested': {},
+    'postcss-conditionals': {},
+    'postcss-math': {},
+    'postcss-focus': {},
+    'postcss-cssnext': {},
+    'postcss-reporter': {
+      clearMessages: true
+    }
+  }
 };
