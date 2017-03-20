f18e284fcfcbb40ef2d547746db1268471207662
Daisy Smith <daisymarie128@gmail.com>
2017-03-20 11:00:15 +0000

    lint

--- example/index.js
+++ example/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import Fullbleed from '../components/fullbleed/fullbleed';
 
 ReactDOM.render(
-	<div>
-		<Fullbleed addClass="home-page__bg" width={200} height={300} ratio={'1:1'}>
-			<img src="https://placekitten.com/300/201" alt="" />
-		</Fullbleed>
-	</div>, document.getElementById('container'));
+  <div>
+    <Fullbleed addClass="home-page__bg" width={200} height={300} ratio={'1:1'}>
+      <img src="https://placekitten.com/300/201" alt="" />
+    </Fullbleed>
+  </div>,
+  document.getElementById('container')
+);
