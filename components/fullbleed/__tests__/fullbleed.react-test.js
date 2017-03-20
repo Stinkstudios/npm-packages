09e925775a25c944f0d1be9a5ec3530e0d4e54d3
Andy Kenward <andy.kenward@stinkstudios.com>
2017-03-20 17:36:16 +0000

    lint + format

--- components/fullbleed/__tests__/fullbleed.react-test.js
+++ components/fullbleed/__tests__/fullbleed.react-test.js
@@ -1,33 +1,29 @@
 import React from 'react';
-import Fullbleed from '../fullbleed';
 import renderer from 'react-test-renderer';
-
 import { mount } from 'enzyme';
 
-const component =
-	<Fullbleed
-		width={100}
-		height={100}
-		ratio="1:1"
-		addClass="home-page__bg"
-	>
-		<img
-			className="home-page__image"
-			src="data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="
-			alt="black square"
-		/>
-	</Fullbleed>;
+import Fullbleed from '../fullbleed';
+
+const component = (
+  <Fullbleed width={100} height={100} ratio="1:1" addClass="home-page__bg">
+    <img
+      className="home-page__image"
+      src="data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="
+      alt="black square"
+    />
+  </Fullbleed>
+);
 
 // Content needs to be deterministic for the test to make sense,
 // so make this a pure function with inline data-uri
 it('renders correctly', () => {
-	const tree = renderer.create(component).toJSON();
+  const tree = renderer.create(component).toJSON();
 
-	expect(tree).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
 });
 
 it('renders the media correctly', () => {
-	const tree = mount(component);
+  const tree = mount(component);
 
-	expect(tree.find('img').length).toBe(1);
+  expect(tree.find('img').length).toBe(1);
 });
