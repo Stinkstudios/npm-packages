cb0a5efd693b6593b070bb01fb4a7489d4795705
James Oldfield <hi@james-oldfield.co>
2017-03-20 17:18:37 +0000

    add tests for invalid ratio format even given string prop

--- utils/validate-ratio.js
+++ utils/validate-ratio.js
@@ -3,6 +3,20 @@ const validateRatio = (props, propName, componentName = "ANONYMOUS") => {
     const ratio = props[propName];
     if (typeof ratio !== "string")
       throw new Error(`Ratio must be a string, got ${typeof ratio}`);
+
+    const rs = ratio.split(":");
+
+    console.log(rs);
+
+    if (rs.length < 2)
+      throw new Error(
+        'Ratio prop must be supplied in the format "W:H", where W and H are a number'
+      );
+
+    if (isNaN(parseInt(rs[0], 10)) || isNaN(parseInt(rs[1], 10)))
+      throw new Error(
+        'Ratio prop must be supplied in the format "W:H", where W and H are a number'
+      );
   }
 
   return null;
