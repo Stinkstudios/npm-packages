47c19381a71e8e6b3ab2f82f64ccd78109bd4c03
James Oldfield <hi@james-oldfield.co>
2017-03-21 11:17:28 +0000

    adhere to new rules in util tests

--- utils/validate-ratio.test.js
+++ utils/validate-ratio.test.js
@@ -1,20 +1,20 @@
-import { validateRatio } from "./validate-ratio";
+import { validateRatio } from './validate-ratio';
 
 const error = 'Ratio prop must be supplied in the format "W:H", where W and H are a number';
 
 const typeError = type => `Ratio must be a string, got ${type}`;
 
-test("validation of prop type", () => {
-  expect(validateRatio({ ratio: "1:1" }, "ratio")).toBe(null);
+test('validation of prop type', () => {
+  expect(validateRatio({ ratio: '1:1' }, 'ratio')).toBe(null);
   expect(() =>
-    validateRatio({ ratio: 12121 }, "ratio").toThrow(typeError(typeof 12121)));
+    validateRatio({ ratio: 12121 }, 'ratio').toThrow(typeError(typeof 12121)));
 });
 
-test("validity of string format", () => {
-  expect(validateRatio({ ratio: "1:1" }, "ratio")).toBe(null);
-  expect(() => validateRatio({ ratio: "ratio" }, "ratio")).toThrow(error);
-  expect(() => validateRatio({ ratio: "bob:mike" }, "ratio")).toThrow(error);
-  expect(() => validateRatio({ ratio: 12121 }, "ratio")).toThrow(
+test('validity of string format', () => {
+  expect(validateRatio({ ratio: '1:1' }, 'ratio')).toBe(null);
+  expect(() => validateRatio({ ratio: 'ratio' }, 'ratio')).toThrow(error);
+  expect(() => validateRatio({ ratio: 'bob:mike' }, 'ratio')).toThrow(error);
+  expect(() => validateRatio({ ratio: 12121 }, 'ratio')).toThrow(
     typeError(typeof 12121)
   );
 });
