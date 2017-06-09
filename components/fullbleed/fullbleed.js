3829fbdca98cf6927822dc477d9cd854b6856b72
Matteo Belfiore <matteo.belfiore@stinkstudios.com>
2017-06-09 11:47:14 +0100

    Fix, edit, improve, and add comments to current Components

--- components/fullbleed/fullbleed.js
+++ components/fullbleed/fullbleed.js
@@ -1,14 +1,53 @@
-import React, { Component, PropTypes } from 'react';
-import { validateClassNames } from '../../utils/validate-class';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './fullbleed.css';
 
 class Fullbleed extends Component {
-  componentWillMount() {
-    const { ratio } = this.props;
-    const ratioValues = ratio.split(':');
-    this.childRatio = ratioValues[0] / ratioValues[1];
+  constructor(props) {
+    super(props);
+    this.state = {
+      ratio: 1
+    };
   }
+
+  componentWillMount() {
+    const { assetRatio } = this.props;
+    const ratioValues = assetRatio.split(':');
+    this._assetRatio = ratioValues[0] / ratioValues[1];
+  }
+
+  componentDidMount() {
+    const { width, height } = this.props;
+    this._onResize();
+    if (!width && !height) {
+      window.addEventListener('resize', this._onResize, false);
+    }
+  }
+
+  componentWillReceiveProps({ width, height }) {
+    if (
+      width &&
+      height &&
+      (width !== this.props.width || height !== this.props.height)
+    ) {
+      this._onResize();
+    }
+  }
+
+  shouldComponentUpdate(nextProps, { ratio }) {
+    return ratio !== this.state.ratio;
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._onResize, false);
+  }
+
+  _onResize = () => {
+    this.setState({
+      ratio: this._el.clientWidth / this._el.clientHeight
+    });
+  };
 
   renderChildren(blockName) {
     const { children } = this.props;
@@ -16,7 +55,6 @@ class Fullbleed extends Component {
       if (child.type !== 'img' && child.type !== 'video') {
         throw new Error('Children must be of type img or of type video');
       }
-
       return React.cloneElement(child, {
         className: `${blockName}__item o-media__${child.type}`
       });
@@ -24,17 +62,21 @@ class Fullbleed extends Component {
   }
 
   render() {
-    const { addClass, width, height } = this.props;
+    const { classNames } = this.props;
+    const { ratio } = this.state;
 
     const blockName = 'fullbleed';
-    const classes = [blockName];
-    if (addClass) classes.push(addClass);
-    const browserRatio = width / height;
-    if (browserRatio < this.childRatio) classes.push('is-below-ratio');
-    classes.push('o-media');
+    const blockNames = [blockName];
+    if (classNames) blockNames.concat(classNames);
+    if (ratio < this._assetRatio) blockNames.push('is-below-ratio');
 
     return (
-      <div className={classes.join(' ')}>
+      <div
+        ref={el => {
+          this._el = el;
+        }}
+        className={blockNames.join(' ')}
+      >
         {this.renderChildren(blockName)}
       </div>
     );
@@ -42,15 +84,28 @@ class Fullbleed extends Component {
 }
 
 Fullbleed.propTypes = {
-  addClass: validateClassNames,
+  /** Add additional classe names to the component */
+  classNames: PropTypes.array,
+  /** HTMLElement, either an 'img' or a 'video' */
   children: PropTypes.node.isRequired,
-  ratio: PropTypes.string.isRequired,
-  width: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired
+  /** The aspect ratio of the asset passed as a child */
+  assetRatio: PropTypes.string.isRequired,
+  /** When used, it must be passed in pair with the `height` prop.
+   * It represents width of the parent element containing `Fullbleed`.
+   * It could be the viewport width, if `Fullbleed` is used on fullscreen
+   */
+  width: PropTypes.number,
+  /** When used, it must be passed in pair with the `width` prop.
+   * It represents height of the parent element containing `Fullbleed`.
+   * It could be the viewport height, if `Fullbleed` is used on fullscreen
+   */
+  height: PropTypes.number
 };
 
 Fullbleed.defaultProps = {
-  addClass: null
+  classNames: null,
+  width: null,
+  height: null
 };
 
 export default Fullbleed;
