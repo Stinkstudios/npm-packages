97b1e1d0066e39fa69da04adbe11ed3dce57a88a
Patrick Arminio <patrick.arminio@gmail.com>
2017-11-16 12:05:49 +0000

    Initial commit

---
+++ packages/cloudfront-auth/__tests__/utils/generate-request.ts
@@ -0,0 +1,60 @@
+const template = {
+  Records: [
+    {
+      cf: {
+        config: {
+          distributionId: "EDFDVBD6EXAMPLE",
+          requestId: "MRVMF7KydIvxMWfJIglgwHQwZsbG2IhRJ07sn9AkKUFSHS9EXAMPLE=="
+        },
+        request: {
+          clientIp: "2001:0db8:85a3:0:0:8a2e:0370:7334",
+          headers: {
+            host: [
+              {
+                key: "Host",
+                value: "d111111abcdef8.cloudfront.net"
+              }
+            ],
+            "user-agent": [
+              {
+                key: "User-Agent",
+                value: "curl/7.51.0"
+              }
+            ]
+          },
+          method: "GET",
+          querystring: "size=large",
+          uri: "/picture.jpg"
+        }
+      }
+    }
+  ]
+};
+
+interface IGenerateRequestParams {
+  distributionId?: string;
+  clientIp?: string;
+  uri?: string;
+}
+
+const generateRequest = (
+  { distributionId, clientIp, uri }: IGenerateRequestParams = {}
+) => {
+  const request = {
+    ...template
+  };
+
+  if (distributionId) {
+    request.Records[0].cf.config.distributionId = distributionId;
+  }
+  if (clientIp) {
+    request.Records[0].cf.request.clientIp = clientIp;
+  }
+  if (uri) {
+    request.Records[0].cf.request.uri = uri;
+  }
+
+  return request;
+};
+
+export default generateRequest;
