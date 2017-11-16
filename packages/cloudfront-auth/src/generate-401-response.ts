512525596745b4b8fc4576ee9308770d87f2d301
Patrick Arminio <patrick.arminio@gmail.com>
2017-11-16 17:34:56 +0000

    Add main entry point

---
+++ packages/cloudfront-auth/src/generate-401-response.ts
@@ -0,0 +1,25 @@
+export interface IGenerate401ResponseParams {
+  realm: string;
+}
+
+const generate401Response = ({ realm }: IGenerate401ResponseParams) => ({
+  body: "401 Authorization Required",
+  headers: {
+    "content-type": [
+      {
+        key: "Content-Type",
+        value: "text/plain; charset=utf-8"
+      }
+    ],
+    "www-authenticate": [
+      {
+        key: "WWW-Authenticate",
+        value: `Basic realm="${realm}"`
+      }
+    ]
+  },
+  status: "401",
+  statusDescription: "Authorization Required"
+});
+
+export default generate401Response;
---
+++ packages/cloudfront-auth/src/index.ts
@@ -0,0 +1,25 @@
+import generate401 from "./generate-401-response";
+import isAuthenticated, { IIsAuthenticatedConfig } from "./is-authenticated";
+
+import { ICloudFrontRequest, IRequest } from "./cloudfront-request";
+
+const processRequest = (
+  configuration: IIsAuthenticatedConfig,
+  event: ICloudFrontRequest,
+  callback: (err: Error | null, response: any) => void
+) => {
+  const authenticated = isAuthenticated(event, configuration);
+
+  if (authenticated) {
+    return callback(null, event.Records[0].cf.request);
+  }
+
+  return callback(
+    null,
+    generate401({
+      realm: "Authentication required"
+    })
+  );
+};
+
+export default processRequest;
