54641bf7bfdb9902d0b12004136d70b5c25c8e4b
Patrick Arminio <patrick.arminio@gmail.com>
2017-11-17 12:43:09 +0000

    Use prettierrc to configure prettier

--- packages/cloudfront-auth/__tests__/is-authenticated-spec.ts
+++ packages/cloudfront-auth/__tests__/is-authenticated-spec.ts
@@ -1,60 +1,60 @@
-import generateAuthHeader from "./utils/generate-auth-header";
-import generateRequest from "./utils/generate-request";
+import generateAuthHeader from './utils/generate-auth-header';
+import generateRequest from './utils/generate-request';
 
-import isAuthenticated from "../src/is-authenticated";
+import isAuthenticated from '../src/is-authenticated';
 
-test("returns false by default", () => {
+test('returns false by default', () => {
   const request = generateRequest();
 
   expect(isAuthenticated(request)).toBe(false);
 });
 
-test("returns true when distribution id is not enabled", () => {
+test('returns true when distribution id is not enabled', () => {
   const request = generateRequest({
-    distributionId: "NOT_ENABLED_ID"
+    distributionId: 'NOT_ENABLED_ID',
   });
 
   expect(
     isAuthenticated(request, {
-      enabledDistributions: ["ENABLED_ID"]
+      enabledDistributions: ['ENABLED_ID'],
     })
   ).toBe(true);
 });
 
-test("returns true when the IP is whitelisted", () => {
+test('returns true when the IP is whitelisted', () => {
   const request = generateRequest({
-    clientIp: "127.0.0.1"
+    clientIp: '127.0.0.1',
   });
 
   expect(
     isAuthenticated(request, {
-      whitelistedIPs: ["127.0.0.1"]
+      whitelistedIPs: ['127.0.0.1'],
     })
   ).toBe(true);
 });
 
-test("returns true when has valid username and password", () => {
-  const auth = generateAuthHeader("username", "password");
+test('returns true when has valid username and password', () => {
+  const auth = generateAuthHeader('username', 'password');
 
   const request = generateRequest({
     headers: {
       authorization: [
         {
-          key: "Authorization",
-          value: auth
-        }
-      ]
-    }
+          key: 'Authorization',
+          value: auth,
+        },
+      ],
+    },
   });
 
   expect(
     isAuthenticated(request, {
       validCredentails: [
         {
-          password: "password",
-          username: "username"
-        }
-      ]
+          password: 'password',
+          username: 'username',
+        },
+      ],
     })
   ).toBe(true);
 });
