6219ba5798b02d02cda95b64bb29ca51503f741d
Andy Kenward <hello@andykenward.com>
2018-10-26 10:50:57 +0100

    feat(react-scripts): moved postcss config to own file

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -80,23 +80,9 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
       options: cssOptions,
     },
     {
-      // Options for PostCSS as we reference these options twice
-      // Adds vendor prefixing based on your specified browser support in
-      // package.json
+      // See postcss.config.js for options & plugins
       loader: require.resolve('postcss-loader'),
       options: {
-        // Necessary for external CSS imports to work
-        // https://github.com/facebook/create-react-app/issues/2677
-        ident: 'postcss',
-        plugins: () => [
-          require('postcss-flexbugs-fixes'),
-          require('postcss-preset-env')({
-            autoprefixer: {
-              flexbox: 'no-2009',
-            },
-            stage: 3,
-          }),
-        ],
         sourceMap: shouldUseSourceMap,
       },
     },
