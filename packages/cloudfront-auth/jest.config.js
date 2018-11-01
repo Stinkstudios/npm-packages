04f6cebb422a7327558d64526bf02949c51d2e20
Andy Kenward <andy.kenward@stinkstudios.com>
2018-11-01 14:26:05 +0000

    feat: coverage to codecov.io

--- packages/cloudfront-auth/jest.config.js
+++ packages/cloudfront-auth/jest.config.js
@@ -1,6 +1,13 @@
+'use strict';
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/?(*.)(spec|test).{ts}'],
   roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx,mjs}',
+    '!src/**/*.{module.css.d.ts,css.d.ts}',
+    '!src/__tests__/**/*.{module.css.d.ts,css.d.ts,ts}',
+  ],
 };
