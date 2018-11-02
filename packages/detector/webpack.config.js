2135ef45a55a91298000bacf0c3b35cc0c4cc9f7
Andy Kenward <andy.kenward@stinkstudios.com>
2018-11-02 11:25:46 +0000

    feat(refactor): output to lib

--- packages/detector/webpack.config.js
+++ packages/detector/webpack.config.js
@@ -1,23 +1,25 @@
-var webpack = require('webpack')
-var path = require('path')
+'use strict';
+
+var webpack = require('webpack');
+var path = require('path');
 var pkg = require('./package.json');
 
 module.exports = {
-    context: path.join(__dirname, './src'),
-    entry: {
-        js: './index.js'
-    },
-    output: {
-        path: path.join(__dirname, './dist'),
-        filename: 'detector.min.js',
-    },
-    resolve: {
-        extensions: ['', '.js']
-    },
-    plugins: [
-        // new webpack.DefinePlugin({
-        //     __VERSION__: JSON.stringify(pkg.version),
-        // }),
-        new webpack.optimize.UglifyJsPlugin({minimize: true})
-    ]
+  context: path.join(__dirname, './src'),
+  entry: {
+    js: './index.js',
+  },
+  output: {
+    path: path.join(__dirname, './lib'),
+    filename: 'detector.min.js',
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(pkg.version),
+    }),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+  ],
 };
