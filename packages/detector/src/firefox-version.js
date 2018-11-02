e4d764c779c08bfe7de6bc36b95357c5cc07ff27
Andy Kenward <andy.kenward@stinkstudios.com>
2018-11-02 14:00:08 +0000

    fix: lint errors & warnings

--- packages/detector/src/firefox-version.js
+++ packages/detector/src/firefox-version.js
@@ -2,7 +2,7 @@ var UA = require('./ua');
 var IS_FIREFOX = require('./is-firefox');
 
 var FIREFOX_VERSION = (function () {
-	if (!IS_FIREFOX) return -1;
+	if (!IS_FIREFOX) {return -1;}
 	return parseFloat(UA.slice(UA.indexOf('Firefox') + 8));
 })();
 
