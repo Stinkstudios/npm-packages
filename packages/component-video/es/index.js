7b24e1de61ca23de0bac4ee5629e45cb538ef3d1
Andy Kenward <andy.kenward@stinkstudios.com>
2018-11-07 16:58:02 +0000

    feat: git ignore build output folders
    As these are built on a published version and used on npm

--- packages/component-video/es/index.js
+++
@@ -1,7 +0,0 @@
-import VideoPlayer from './VideoPlayer';
-
-if (typeof window !== 'undefined') {
-	window.VideoPlayer = VideoPlayer;
-}
-
-export default VideoPlayer;
--- packages/component-video/lib/utils/math.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-exports.clamp = clamp;
-function clamp(t, a, b) {
-	// eslint-disable-line import/prefer-default-export
-	return Math.max(Math.min(t, b), a);
-}
