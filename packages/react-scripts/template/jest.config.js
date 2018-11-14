6f150cd6bd3279c17746d040dc825e244b574eec
Andy Kenward <andy.kenward@stinkstudios.com>
2018-11-14 14:45:25 +0000

    fix: jest external config file & cli test

--- packages/react-scripts/template/jest.config.js
+++ packages/react-scripts/template/jest.config.js
@@ -1,33 +1,32 @@
-'use strict';
-
-const paths = require('../config/paths');
-
 module.exports = {
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
-  resolver: require.resolve('jest-pnp-resolver'),
-  setupFiles: [require.resolve('react-app-polyfill/jsdom')],
-  setupTestFrameworkScriptFile: undefined,
+  resolver: 'jest-pnp-resolver',
+  setupFiles: ['react-app-polyfill/jsdom'],
   testMatch: [
-    '<rootDir>/template/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/template/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
   ],
   testEnvironment: 'jsdom',
   testURL: 'http://localhost',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/config/jest/babelTransform.js',
-    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)':
-      '<rootDir>/config/jest/fileTransform.js',
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^(?!.*\\.(js|jsx|ts|tsx|json)$)': 'jest-file',
   },
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$'],
   moduleNameMapper: {
     '^react-native$': 'react-native-web',
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.module\\.(css|sass|scss)$': 'jest-css-modules',
   },
-  moduleFileExtensions: [...paths.moduleFileExtensions, 'node'].filter(
-    ext => !ext.includes('mjs')
-  ),
+  moduleFileExtensions: [
+    'web.js',
+    'js',
+    'web.ts',
+    'ts',
+    'web.tsx',
+    'tsx',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
 };
