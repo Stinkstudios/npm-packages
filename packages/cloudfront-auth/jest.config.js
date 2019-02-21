48bb9974631a41db5a56dca3a5ec8ae4bef47803
Andy Kenward <hello@andykenward.com>
2019-02-21 11:30:09 +0000

    feat: jest v24 support

--- packages/cloudfront-auth/jest.config.js
+++ packages/cloudfront-auth/jest.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/?(*.)(spec|test).{ts}'],
+  testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)'],
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js,jsx,mjs}',
