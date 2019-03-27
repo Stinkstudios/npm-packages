bd85a1d883f1c7092ec7fe68ad9fc7dc319a0ff4
Andy Kenward <andy.kenward@stinkstudios.com>
2019-03-27 10:50:09 +0000

    fix: use kebabCase and change rule in files
    As the original react-scripts doesn’t stick to a filename conventition and we want to stick to kebabCase in a project i’ve changed the rule in files as needed.

--- packages/react-scripts/config/webpackDevServer.config.js
+++ packages/react-scripts/config/webpackDevServer.config.js
@@ -6,6 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 // @remove-on-eject-end
+/* eslint "unicorn/filename-case": ['error', { case: 'camelCase' }] */
 'use strict';
 
 const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
