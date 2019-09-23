25a1a12cfc25f0bb5a792901a328847d29b5e546
emielvanbetsbrugge <emiel.vanbetsbrugge@gmail.com>
2019-09-23 14:23:45 -0400

    fix(template): fixed template for non ts react projects

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -7,4 +7,4 @@ if (process.env.NODE_ENV === 'production') {
   console.log('BUILD NUM:', process.env.BUILD_NUM); // eslint-disable-line no-console
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
